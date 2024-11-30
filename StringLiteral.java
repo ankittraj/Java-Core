8337de10f7c1c3d5566711f44f7f25a80bba65a5
ANKIT RAJ <110198496+ankittraj@users.noreply.github.com>
2024-11-30 23:45:18 +0530

    Add files via upload

---
+++ StringLiteral.java
@@ -0,0 +1,17 @@
+public class StringLiteral {
+    public static void main(String[] args) {
+        String a = "Ankit Raj";//String Literal
+        String b = "104";//String Literal
+        String c = "a";//String Literal
+        String d = "4+5";//String Literal
+        String e = "4<5";//String Literal
+        String f = "#Raj@354875";//String Literal
+    
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(f);
+    }
+}
