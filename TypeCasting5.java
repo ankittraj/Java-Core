6b3b3412c9a405ac58df91ceef40ada448a385ed
ANKIT RAJ <110198496+ankittraj@users.noreply.github.com>
2024-12-02 23:55:53 +0530

    Add files via upload

---
+++ TypeCasting5.java
@@ -0,0 +1,8 @@
+public class TypeCasting5{
+    static public void main(String [] rahul){
+        byte a=120;
+        //a = a+10;//error
+        a = (byte)(a+10);
+        System.out.println(a);
+    }
+}
