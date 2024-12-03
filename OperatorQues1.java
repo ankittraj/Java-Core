5fd5edb4fc1a16743a938466fa7d6944cfdc58e2
ANKIT RAJ <110198496+ankittraj@users.noreply.github.com>
2024-12-03 23:50:30 +0530

    Add files via upload

---
+++ OperatorQues1.java
@@ -0,0 +1,14 @@
+class OperatorQues1{
+    static public void main(String [] s){
+        int a=7/2;
+        System.out.println(a); 
+        double b=7/2;
+        System.out.println(b);
+        double c=7.0/2;
+        System.out.println(c);
+        double d=7/2.0;
+        System.out.println(d);
+        double e=7.0/2.0;
+        System.out.println(e);
+    }
+}
