6a342ac2be71ea59296203c46dd417fc0eede771
ANKIT RAJ <110198496+ankittraj@users.noreply.github.com>
2024-12-03 23:24:46 +0530

    Add files via upload

---
+++ TypeCasting6.java
@@ -0,0 +1,9 @@
+public class TypeCasting6{
+    static public void main(String [] rahul){
+        byte a=104;
+        //char b=a;//error
+        char b=(char)a;
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
