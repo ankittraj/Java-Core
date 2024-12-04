604b8ca7ca11be5582e3420ea37f8970f547eb4f
ANKIT RAJ <110198496+ankittraj@users.noreply.github.com>
2024-12-04 23:15:00 +0530

    Add files via upload

---
+++ OperatorQues2.java
@@ -0,0 +1,22 @@
+class OperatorQues2{
+    static public void main(String [] s){
+        int a;
+        a=7/2;
+        System.out.println(a); 
+        a=-7/2;
+        System.out.println(a); 
+        a=7/-2;
+        System.out.println(a);
+        a=-7/-2;
+        System.out.println(a);  
+
+        a=7%2;
+        System.out.println(a); 
+        a=-7%2;
+        System.out.println(a); 
+        a=7%-2;
+        System.out.println(a);
+        a=-7%-2;
+        System.out.println(a);  
+    }
+}
