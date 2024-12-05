b84d5ffd085a3daa3017e28e6e0434cb988e9d60
ANKIT RAJ <110198496+ankittraj@users.noreply.github.com>
2024-12-05 19:25:40 +0530

    Add files via upload

---
+++ OperatorQues4.java
@@ -0,0 +1,28 @@
+class OperatorQues4{
+    static public void main(String [] s){
+        int a=10;
+        a+=5;
+        System.out.println(a); 
+
+        a=10;
+        a/=4;
+        System.out.println(a);
+
+        a=15;
+        a%=4;
+        System.out.println(a);  
+
+        a=15;
+        a%=24;
+        System.out.println(a);  
+
+        a=15;
+        //a=%4;//error  
+        a=+4;
+        System.out.println(a); 
+
+        a=15;
+        a=-4;
+        System.out.println(a);
+    }
+}
