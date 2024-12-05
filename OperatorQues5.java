0a03df1094e68e1d73862843b35c697860079072
ANKIT RAJ <110198496+ankittraj@users.noreply.github.com>
2024-12-05 19:26:08 +0530

    Add files via upload

---
+++ OperatorQues5.java
@@ -0,0 +1,25 @@
+class OperatorQues5{
+    static public void main(String [] s){
+        byte a=10;
+        a++;
+        System.out.println(a);
+        short b=10;
+        b++;
+        System.out.println(b);
+        int c=10;
+        c++;
+        System.out.println(c);
+        long d=10;
+        d++;
+        System.out.println(d);
+        float e=10.2f;
+        e++;
+        System.out.println(e);
+        double f=10.2;
+        f++;
+        System.out.println(f);
+        char g='m';
+        g++;
+        System.out.println(g); 
+    }
+}
