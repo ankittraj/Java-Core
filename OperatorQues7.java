76d3d09ffb1ecd500ec201d978750cd2902b4193
ANKIT RAJ <110198496+ankittraj@users.noreply.github.com>
2024-12-06 21:29:50 +0530

    Add files via upload

---
+++ OperatorQues7.java
@@ -0,0 +1,9 @@
+class OperatorQues7{
+    static public void main(String [] s){
+        int a=10,b;
+        // b=a++;
+        b=++a;
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
