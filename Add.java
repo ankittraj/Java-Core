bb2b011541a9a419e836209120a97de30ba01837
ANKIT RAJ <110198496+ankittraj@users.noreply.github.com>
2024-12-08 23:19:41 +0530

    Add files via upload

---
+++ Add.java
@@ -0,0 +1,14 @@
+public class Add {
+	public static void main(String[] args) {
+		java.util.Scanner sc = new java.util.Scanner(System.in);
+		System.out.println("Enter 1st nos.:");
+		int a = sc.nextInt();
+		System.out.println("Enter 2nd nos.:");
+		int b= sc.nextInt();
+		int r = a+b;
+		System.out.print(r);
+		
+		sc.close();
+	}
+
+}
